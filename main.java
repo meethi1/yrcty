be01e3da9ee2c81f0f63def4f1e5dc58f96d5d07
meethi1 <88002948+meethi1@users.noreply.github.com>
2024-07-13 20:57:39 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
+import service.FileIO;
+import service.Priority;
+import service.Status;
+import service.Task_Manager;
+import models.Task;
+public class Main {
+	
+	public static void main(String[] args) {
+		Task_Manager.run();
+	}
+
+}
